f1db43539702945277b9f5381dfe48e6e71ae048
Pankaj <pankajkumarpk9852@gamil.com>
2023-01-25 13:31:15 +0530

    animation added

--- app.js
+++ app.js
@@ -14,9 +14,14 @@ closeNav.addEventListener('click',closeMenu);
 
 navOverlay.addEventListener('click',closeMenu);
 
+let screenSize = window.innerWidth;
+
 navLinks.forEach((link)=>{
-    link.addEventListener('click',closeMenu);
+    if(screenSize<= 650){
+        link.addEventListener('click',closeMenu);
+    }
 });
+
 
 function closeMenu(){
     navbar.style.transform = "scale(0)";
@@ -24,11 +29,63 @@ function closeMenu(){
 }
 
 // for photo overlay 
-let myphoto = document.querySelector(".my-photo");
-let photoWidth = myphoto.offsetWidth;
-let photoHeight = myphoto.offsetHeight;
+window.addEventListener('load',()=>{
+    let myphoto = document.querySelector(".my-photo");
+    let photoWidth = myphoto.offsetWidth;
+    let photoHeight = myphoto.offsetHeight;
 
-let photoOverlay = document.querySelector(".photoOverlay");
+    let photoOverlay = document.querySelector(".photoOverlay");
 
-photoOverlay.style.width = photoWidth + "px";
-photoOverlay.style.height = photoHeight + "px";
+    photoOverlay.style.width = photoWidth + "px";
+    photoOverlay.style.height = photoHeight + "px";
+    
+})
+
+
+
+// intersection animation 
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('slide-right');            
+        } else{
+            entry.target.classList.remove('slide-right');
+        }
+
+    });
+});
+
+const leftAnimated = document.querySelectorAll(".hidden-left");
+
+leftAnimated.forEach((item,index) => {
+
+    observer.observe(item);
+    
+    for(let i = 0;i<10;i++){
+        if(index == i)
+            item.style.transitionDelay = i * 0.2 + "s";
+    }
+    
+});
+
+// Intersection animation for my photo 
+const myImgObserver = new IntersectionObserver((rightEntries) => {
+    rightEntries.forEach((rightEntry) => {
+        if(rightEntry.isIntersecting){
+            rightEntry.target.classList.add('slide-left');
+        } else{
+            rightEntry.target.classList.remove('slide-left');
+        }
+    });
+});
+
+const rightAnimated = document.querySelectorAll(".hidden-right");
+
+rightAnimated.forEach((rightItem) => {
+
+    myImgObserver.observe(rightItem);
+    
+});
